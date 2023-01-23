c97e36cc0d4c9dae1d4dddbd91c4812d7cb8fe49
Hailey Francis <n01402670@unf.edu>
2023-01-23 18:44:53 -0500

    started inquirer construction and looping of questions

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern")
 
+let employees = [];
+
 //TODO: 
 //using inquirer,
     //get the team manager's
@@ -26,7 +28,36 @@ const Intern = require("./lib/Intern")
                 //email
                 //school
     //when complete, html file generates
+
+//initates program
 function init(){
+    createManager();
+}
+
+//selects between Engineer, Intern, or Exit
+function selectEmployeeType(){
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "add_Empl_opt",
+            message: "What type of Employee would you like to add?",
+            choices: ["Engineer", "Intern", "Finished"]
+        }
+    ])
+    .then((data) => {
+        switch(data.add_Empl_opt){
+            case "Engineer": createEngineer(); break;
+            case "Intern": createIntern(); break;
+            case "Finished": return;
+            default: console.log("[ERROR] Valid option not selected. (" + option + ")");
+        }
+    })
+    
+}
+
+//creates manager using inquirer, pushes manager to employee arr
+function createManager(){
     inquirer
     .prompt([
         {
@@ -55,11 +86,56 @@ function init(){
         }
     ])
     .then((data) => {
-        console.log("inquirer done");
         const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOfficeNum);
-        console.log(manager);
+        employees.push(manager);
+        selectEmployeeType();
+    })
+    .catch()
+}
+
+//creates engineer using inquirer, pushes engineer to employee arr
+function createEngineer(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "eng_Name",
+            message: "What is the engineer's name?",
+            validate: (value) => {if(value){return true}else{return "Please enter a response."}}
+        },
+        {
+            type: "input",
+            name: "eng_ID",
+            message: "What is the engineer's ID?",
+            validate: (value) => {if(value){return true}else{return "Please enter a response."}}
+        },
+        {
+            type: "input",
+            name: "eng_email",
+            message: "What is the engineer's email address?",
+            validate: (value) => {if(value){return true}else{return "Please enter a response."}}
+        },
+        {
+            type: "input",
+            name: "eng_github",
+            message: "What is the engineer's Github username?",
+            validate: (value) => {if(value){return true}else{return "Please enter a response."}}
+        },
+    ])
+    .then((data) => {
+        const engineer =  new Engineer(data.eng_Name, data.eng_ID, data.eng_email, data.eng_github);
+        employees.push(engineer);
+        selectEmployeeType();
     })
 }
+
+//creates intern using inquirer, pushes engineer to employee arr
+function createIntern(){
+    console.log("Intern");
+}
+
+
+
 
 //TODO:
 //generate HTML file from user's input data
