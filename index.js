b70dad6452a27c44d93172e83812266127f060ee
Hailey Francis <n01402670@unf.edu>
2023-01-22 19:33:53 -0500

    added file structure

---
+++ index.js
@@ -0,0 +1,30 @@
+//packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//TODO: 
+//using inquirer,
+    //get the team manager's
+        //name
+        //employee ID
+        //email address
+        //office number
+
+    //present a menu option that is able to loop
+        //choose engineer or intern
+            //choosing engineer:
+                //name
+                //id
+                //email
+                //github
+            //choose intern:
+                //name
+                //id
+                //email
+                //school
+    //when complete, html file generates
+
+//TODO:
+//generate HTML file from user's input data
+    //clicking on an email address, default email program opens and populates TO field
+    //clicking on a github takes you to the github page
